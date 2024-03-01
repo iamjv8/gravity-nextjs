5983f72506811693c18d81f17462c30118545514
Jayesh Vekariya <iamjv8@gmail.com>
2024-03-01 05:36:27 +0000

    Basic Login completed

--- app/login/page.js
+++ app/login/page.js
@@ -1,29 +1,19 @@
 "use client";
-import { useEffect } from "react";
 import {
   AuthorizerProvider,
   Authorizer,
 } from "@authorizerdev/authorizer-react";
-import { Flex, Button, Checkbox, Form, Input, Typography } from "antd";
-
+import { useRouter } from "next/navigation";
+import { Flex, Typography } from "antd";
 import "./login.scss";
 
-const Profile = () => {};
-
 const LoginPage = () => {
-  const onFinish = (values) => {
-    console.log("Success:", values);
-  };
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
+  const router = useRouter();
+  const onLoginSuccess = (values) => {
+    router.push("/dashboard");
   };
 
   const { Title } = Typography;
-
-  // let redirectURL = "";
-  // useEffect(() => {
-  //   redirectURL = `${window.location.origin}/dashboard`;
-  // });
   return (
     <div className="main">
       <div className="container">
@@ -34,64 +24,13 @@ const LoginPage = () => {
           <AuthorizerProvider
             config={{
               authorizerURL: "https://authorizer-oknc.onrender.com",
-              redirectURL: "https://gravity-nextjs.onrender.com",
+              redirectURL: "https://gravity-nextjs.onrender.com/dashboard",
               clientID: "13371d2a-ba7e-46f3-bb4d-e23d16b0b5a6", // obtain your client id from authorizer dashboard
               extraHeaders: {}, // Optional JSON object to pass extra headers in each authorizer requests.
             }}
           >
-            <Authorizer />
-            <Profile />
+            <Authorizer onLogin={onLoginSuccess} />
           </AuthorizerProvider>
-          {/* <Form
-            name="basic"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-            className="login-form"
-          >
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your username!",
-                },
-              ]}
-            >
-              <Input placeholder="Username" size="large" />
-            </Form.Item>
-
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your password!",
-                },
-              ]}
-            >
-              <Input.Password placeholder="Password" size="large" />
-            </Form.Item>
-
-            <Form.Item
-              name="remember"
-              valuePropName="checked"
-              wrapperCol={{
-                span: 16,
-              }}
-            >
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item>
-              <Button type="primary" htmlType="submit" block>
-                Login
-              </Button>
-            </Form.Item>
-          </Form> */}
         </Flex>
       </div>
     </div>
